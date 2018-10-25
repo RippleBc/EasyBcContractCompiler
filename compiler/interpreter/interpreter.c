e6ba7d306a9e0f68806a08228ca62248415fa706
ChineseFish <565828928@qq.com>
2018-10-25 09:17:47 +0800

    comment

--- compiler/interpreter/interpreter.c
+++ compiler/interpreter/interpreter.c
@@ -13,13 +13,13 @@ List cp;
 static int label_tree_index = 0;
 static List label_tree_queue[256];
 
-void interpret(List asts)
+void interpret(List dag)
 {
     ptab = Global_symtab;
     Tree t;
     
     /*  */
-    for(cp = asts->link; cp != NULL; cp = cp->link)
+    for(cp = dag->link; cp != NULL; cp = cp->link)
     {
       t = (Tree)(cp->x);
       if(generic(t->op) == LABEL)
@@ -29,7 +29,7 @@ void interpret(List asts)
     }
 
     /*  */
-    for(cp = asts->link; cp != NULL; cp = cp->link)
+    for(cp = dag->link; cp != NULL; cp = cp->link)
     {
       node_process((Tree)(cp->x));
       if(cp == NULL)
@@ -62,29 +62,29 @@ void jump_to_label(Symbol label)
   }
 }
 
-void node_process(Tree ast)
+void node_process(Tree node)
 {
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
     case INCR:
     {
       /* */
-      node_process(ast->kids[0]);
+      node_process(node->kids[0]);
       /* */
-      ast->kids[0]->u.generic.sym->v.i++;
+      node->kids[0]->u.generic.sym->v.i++;
     }
     break;
     case DECR:
     {
       /*  */
-      node_process(ast->kids[0]);
+      node_process(node->kids[0]);
       /*  */
-      ast->kids[0]->u.generic.sym->v.i--;
+      node->kids[0]->u.generic.sym->v.i--;
     }
     break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
   case LABEL:
   {
@@ -93,40 +93,40 @@ void node_process(Tree ast)
   break;
   case JUMP:
   {
-    jump_to_label(ast->u.generic.sym);
+    jump_to_label(node->u.generic.sym);
   }
   break;
   case COND:
   {
-    node_process(ast->kids[0]);
+    node_process(node->kids[0]);
 
-    if((ast->kids[0]->u.generic.val.i == 0 && ast->u.cond_jump.true_or_false == false) || 
-      (ast->kids[0]->u.generic.val.i != 0 && ast->u.cond_jump.true_or_false == true))
+    if((node->kids[0]->u.generic.val.i == 0 && node->u.cond_jump.true_or_false == false) || 
+      (node->kids[0]->u.generic.val.i != 0 && node->u.cond_jump.true_or_false == true))
     {
-      jump_to_label(ast->u.cond_jump.label);
+      jump_to_label(node->u.cond_jump.label);
     }
   }
   break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
   case SYS:
   {
-    if (ast->kids[0] == NULL)
+    if (node->kids[0] == NULL)
     {
       /* 没有参数 */
     }
     else {
       /*  */
-      node_process(ast->kids[0]);
+      node_process(node->kids[0]);
       /* 有参数 */
-      switch (ast->u.sys.sys_id)
+      switch (node->u.sys.sys_id)
       {
         case pWRITELN:
         {
           /* 参数对应的符号 */
-          p = ast->kids[0]->u.generic.sym;
+          p = node->kids[0]->u.generic.sym;
 
           /*  */
           printf("result: %d\n", p->v.i);
@@ -144,74 +144,74 @@ void node_process(Tree ast)
   break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
 	case RIGHT:
 	{
     printf("\nRIGHT, ");
 
 		/* 计算表达式AST节点对应的值 */
-  	if(ast->kids[0]->kids[0] != NULL || ast->kids[0]->kids[1] != NULL)
+  	if(node->kids[0]->kids[0] != NULL || node->kids[0]->kids[1] != NULL)
   	{
-  		node_process(ast->kids[0]);
+  		node_process(node->kids[0]);
   	}
 
     /* 参数对应的符号 */
-    p = ast->u.generic.sym;
+    p = node->u.generic.sym;
 
     /* 参数赋值 */
-    p->v.i = ast->kids[0]->u.generic.val.i;
+    p->v.i = node->kids[0]->u.generic.val.i;
 
   	/* 计算其余参数的值 */
-  	if(ast->kids[1] != NULL)
+  	if(node->kids[1] != NULL)
   	{
-  		node_process(ast->kids[1]);
+  		node_process(node->kids[1]);
   	}
 	}
 	break;
 	case ARG:
 	{
 		/* 计算参数的值 */
-  	if(ast->kids[0] != NULL)
+  	if(node->kids[0] != NULL)
   	{
-  		node_process(ast->kids[0]);
+  		node_process(node->kids[0]);
   	}
 
     /* 参数对应的符号 */
-    p = ast->u.generic.sym;
+    p = node->u.generic.sym;
 
   	/* 参数赋值 */
-  	p->v.i = ast->kids[0]->u.generic.val.i;
+  	p->v.i = node->kids[0]->u.generic.val.i;
 
   	/* 计算其余参数的值 */
-  	if(ast->kids[1] != NULL)
+  	if(node->kids[1] != NULL)
   	{
-  		node_process(ast->kids[1]);
+  		node_process(node->kids[1]);
   	}
 	}
 	break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
     case FIELD:
     {
       
-      ast->u.generic.sym = ast->u.field.field;
+      node->u.generic.sym = node->u.field.field;
     }
     break;
     case ARRAY:
     {
       char ele_name[NAME_LEN];
       /*  */
-      for(p = ast->u.generic.sym->type_link->first; p != NULL; p = p->next)
+      for(p = node->u.generic.sym->type_link->first; p != NULL; p = p->next)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
 
-        snprintf(ele_name, sizeof(ele_name), "%d", ast->kids[0]->u.generic.val.i);
+        snprintf(ele_name, sizeof(ele_name), "%d", node->kids[0]->u.generic.val.i);
         if(is_symbol(p, ele_name))
         {
-          ast->u.generic.sym = p;
+          node->u.generic.sym = p;
           break;
         }
       }
@@ -229,221 +229,221 @@ void node_process(Tree ast)
     break;
     case LOAD:
     {
-      if(ast->kids[0])
+      if(node->kids[0])
       {
-        node_process(ast->kids[0]);
-        ast->u.generic.sym = ast->kids[0]->u.generic.sym;
-        ast->u.generic.val.i = ast->kids[0]->u.generic.sym->v.i;
+        node_process(node->kids[0]);
+        node->u.generic.sym = node->kids[0]->u.generic.sym;
+        node->u.generic.val.i = node->kids[0]->u.generic.sym->v.i;
       }
       else
       {
-        ast->u.generic.val.i = ast->u.generic.sym->v.i;
+        node->u.generic.val.i = node->u.generic.sym->v.i;
       }
     }
     break;
     case ASGN:
     {
       /*  */
-      if(ast->kids[0])
+      if(node->kids[0])
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算表达式AST节点对应的值 */
-      if(ast->kids[1])
+      if(node->kids[1])
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
       
       /* 对应的符号 */
-      p = ast->kids[0]->u.generic.sym;
+      p = node->kids[0]->u.generic.sym;
 
       /* 变量赋值 */
-      p->v.i = ast->kids[1]->u.generic.val.i;
+      p->v.i = node->kids[1]->u.generic.val.i;
     }
     break;
     case CNST:
     {
-      ast->u.generic.val.i = ast->u.generic.sym->v.i;
+      node->u.generic.val.i = node->u.generic.sym->v.i;
     }
     break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
     case ADD:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0])
+      if(node->kids[0])
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1])
+      if(node->kids[1])
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i + ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i + node->kids[1]->u.generic.val.i;
     }
     break;
     case SUB:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i - ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i - node->kids[1]->u.generic.val.i;
     }
     break;
     case MUL:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i * ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i * node->kids[1]->u.generic.val.i;
     }
     break;
     case DIV:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i / ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i / node->kids[1]->u.generic.val.i;
     }
     break;
     case EQ:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i == ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i == node->kids[1]->u.generic.val.i;
     }
     break;
     case NE:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i != ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i != node->kids[1]->u.generic.val.i;
     }
     break;
     case GE:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i >= ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i >= node->kids[1]->u.generic.val.i;
     }
     break;
     case GT:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i > ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i > node->kids[1]->u.generic.val.i;
     }
     break;
     case LE:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i <= ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i <= node->kids[1]->u.generic.val.i;
     }
     break;
     case LT:
     {
       /* 计算左表达式AST节点对应的值 */
-      if(ast->kids[0] != NULL)
+      if(node->kids[0] != NULL)
       {
-        node_process(ast->kids[0]);
+        node_process(node->kids[0]);
       }
 
       /* 计算右表达式AST节点对应的值 */
-      if(ast->kids[1] != NULL)
+      if(node->kids[1] != NULL)
       {
-        node_process(ast->kids[1]);
+        node_process(node->kids[1]);
       }
 
-      ast->u.generic.val.i = ast->kids[0]->u.generic.val.i < ast->kids[1]->u.generic.val.i;
+      node->u.generic.val.i = node->kids[0]->u.generic.val.i < node->kids[1]->u.generic.val.i;
     }
     break;
   }
 
-  switch (generic(ast->op))
+  switch (generic(node->op))
   {
     case HEADER: /* 表示过程以及函数定义的开始 */  
     case TAIL: /* 表示过程以及函数定义的结束 */
